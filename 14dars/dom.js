7220cb35268d8057f3143585cb7de27b9a377003
toirovashabbona <toirovashabbona167@gmail.com>
2024-06-17 10:43:13 +0500

    Add files via upload

---
+++ 14dars/dom.js
@@ -0,0 +1,75 @@
+var body = document.querySelector("body");
+var click = document.getElementById("btn_d");
+click.onclick = function(){
+    body.style.backgroundColor = "black";
+    // alert("Hello pragrammer");
+}
+
+
+var body =document.querySelector("body");
+var click = document.getElementById("btn");
+click.onclick = function(){
+    body.style.backgroundColor = "red";
+    // alert("hello pragrammer")
+}
+
+
+var body = document.querySelector("body");
+var click = document.getElementById("bbtn");
+click.onclick = function(){
+    body.style.backgroundColor = "yellow";
+    // alert("hello ")
+}
+
+
+var body = document.querySelector("body");
+var click = document.getElementById("pp");
+click.onclick = function(){
+    body.style.backgroundColor = "blue";
+    // alert("salom")
+}
+
+var body = document.querySelector("body");
+var click = document.getElementById("p12");
+click.onclick = function(){
+    body.style.backgroundColor = "brown";
+    // alert("salom")
+}
+
+
+var body = document.querySelector("body");
+var click = document.getElementById("iik");
+click.onclick = function(){
+    body.style.backgroundColor = "magenta";
+    // alert("salom")
+}
+
+var body = document.querySelector("body");
+var click = document.getElementById("oo");
+click.onclick = function(){
+    body.style.backgroundColor = "cyan";
+    // alert("hello")
+}
+
+
+var body = document.querySelector("body");
+var click = document.getElementById("h4");
+click.onclick = function(){
+    body.style.backgroundColor = "orange";
+    // alert("hello")
+}
+
+// var body = documet.querySelector("body");
+// var click = documet.getElementById("h5");
+// click.onclick = function(){
+//     body.style.backgroundColor = "pink";
+//     // alert("HELLO")
+// }
+
+
+// var body = document.querySelector("body");
+// var click = document.getElementById("h7");
+// click.onclick =function(){
+//     body.style.backgroundColor = "violet";
+//     // alert("hello")
+// }
